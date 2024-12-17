5111e9c9c8d59894d8bb3436f176b6c56dd980c2
Connor Gannaway <charlesconnorgannaway@gmail.com>
2024-12-17 15:08:13 -0600

    test ui lib reference

--- apps/userweb/src/app/app.tsx
+++ apps/userweb/src/app/app.tsx
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 import NxWelcome from './nx-welcome';
+import { Ui } from '@platform/ui';
 
 const StyledApp = styled.div`
-  // Your style here
+	// Your style here
 `;
 
 export function App() {
-  return (
-    <StyledApp>
-      <NxWelcome title="userweb" />
-    </StyledApp>
-  );
+	return (
+		<StyledApp>
+			<NxWelcome title="userweb" />
+			<Ui />
+		</StyledApp>
+	);
 }
 
 export default App;
