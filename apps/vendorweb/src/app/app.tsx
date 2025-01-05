2f015945deda799d73a5009b93000674263d3a92
Connor Gannaway <charlesconnorgannaway@gmail.com>
2025-01-05 11:33:24 -0600

    example (#4)

--- apps/vendorweb/src/app/app.tsx
+++ apps/vendorweb/src/app/app.tsx
@@ -34,7 +34,7 @@ export function App() {
 					path="/"
 					element={
 						<div>
-							This is the root route.{' '}
+							This is the generated root route.{' '}
 							<Link to="/page-2">Click here for page 2.</Link>
 						</div>
 					}
