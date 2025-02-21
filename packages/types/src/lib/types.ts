bca21249f465a00595a5e39d6bcca68b9ae658a6
Connor Gannaway <charlesconnorgannaway@gmail.com>
2025-02-21 15:14:40 -0500

    update types to align with db

--- packages/types/src/lib/types.ts
+++ packages/types/src/lib/types.ts
@@ -1,21 +1,58 @@
-export type EventData = {
+// Redone to align with database.
+export type Event = {
 	id: string;
-	date: number;
+	vendor: number;
+	venue: number;
 	name: string;
+	type: string;
+	event_datetime: string;
+	description: string;
+	disclaimer: string;
+	basecost: number;
+	num_unique: number;
+	num_ga: number;
+	photo: string;
 };
 
-export type VenueData = {
+export type Venue = {
 	id: string;
-	date: number;
-	location: string;
+	vendor: number;
 	name: string;
-	streetAddr: string;
+	streetaddr: string;
 	zip: number;
 	city: string;
-	stateCode: string;
-	stateName: string;
-	countryCode: string;
-	countryName: string;
-	numUnique: number;
-	numGa: number;
+	state_code: string;
+	state_name: string;
+	country_code: string;
+	country_name: string;
+	num_unique: number;
+	num_ga: number;
+	photo: string;
 };
+
+export type EventCreationFormData = Pick<
+	Event,
+	| 'venue'
+	| 'name'
+	| 'type'
+	| 'event_datetime'
+	| 'description'
+	| 'disclaimer'
+	| 'basecost'
+	| 'num_unique'
+	| 'num_ga'
+>;
+
+export type VenueCreationFormData = Pick<
+	Venue,
+	| 'name'
+	| 'streetaddr'
+	| 'zip'
+	| 'city'
+	| 'state_code'
+	| 'state_name'
+	| 'country_code'
+	| 'country_name'
+	| 'num_unique'
+	| 'num_ga'
+>;
