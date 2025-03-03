70f3b66bce733222eb5300d294c5281514b9bc5a
Connor Gannaway <charlesconnorgannaway@gmail.com>
2025-03-03 13:45:11 -0500

    better logging

--- apps/backend/PhotoUploadEvent.go
+++ apps/backend/PhotoUploadEvent.go
@@ -2,16 +2,41 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/jackc/pgx/v5"
+
+	"backend/query"
+	"backend/shared"
 )
 
+var connStr string
+
+func init() {
+	connStr = shared.InitLambda()
+}
+
 func HandleSQSEvent(ctx context.Context, sqsEvent events.SQSEvent) error {
-    for _, record := range sqsEvent.Records {
-        log.Printf("Received event: %+v", record)
-    }
+	// Connect to the database
+	conn, err := pgx.Connect(ctx, connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+	queries := query.New(conn)
+
+
+	for _, record := range sqsEvent.Records {
+		bs, err := json.Marshal(record)
+		if err != nil {
+			log.Printf("Error marshalling record: %v", err)
+			continue
+		}
+		log.Printf("Received event: %v", string(bs))
+	}
     return nil
 }
 
