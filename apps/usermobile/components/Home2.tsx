9d35578420b81925148df658f27d3c795f446dc3
Christopher Canaday <chrislcanaday@gmail.com>
2025-03-08 01:39:15 -0500

    restructure needed to make it work display events properly. However it properly pulls event information from the data base.

--- apps/usermobile/components/Home2.tsx
+++ apps/usermobile/components/Home2.tsx
@@ -1,14 +1,21 @@
+import { createAccountAbstractionModule } from '@dynamic-labs/client/src/modules/walletsModule/accountAbstractionModule';
+import { isEthereumWallet } from '@dynamic-labs/ethereum';
+import { ContractAddress, ContractABI } from '@platform/blockchain';
 import { useNavigation } from '@react-navigation/native';
-import { View, Button } from 'react-native';
+import { lookup } from 'dns';
+import { tmpdir } from 'os';
+import { View, Button, Text } from 'react-native';
+import { polygonAmoy } from 'viem/chains';
 import { useDynamic } from './DynamicSetup';
+
+// import EventHandler from './EventHandler';
 
 const HomeScreen = () => {
 	// const navigation = useNavigation();
 	const client = useDynamic();
 
 	async function getNFTsInWallet() {
-		// const url = `https://www.oklink.com/api/v5/explorer/address/token-transaction-list-multi?chainShortName=amoy_testnet&address=${client.auth.authenticatedUser?.verifiedCredentials[0].address}&tokenContractAddress=0x578d05ef4BDC8Ee7f0d23b3130BE5F130f88ab29&endBlockHeight=999999999&startBlockHeight=0&limit=100`;
-		const url = `https://www.oklink.com/api/v5/explorer/nft/address-balance-fills?chainShortName=amoy_testnet&address=${client.auth.authenticatedUser?.verifiedCredentials[0].address}&tokenContractAddress=0x578d05ef4BDC8Ee7f0d23b3130BE5F130f88ab29&limit=100&protocolType=token_1155`;
+		const url = `https://www.oklink.com/api/v5/explorer/nft/address-balance-fills?chainShortName=amoy_testnet&address=${client.auth.authenticatedUser?.verifiedCredentials[0].address}&tokenContractAddress=${ContractAddress}&limit=100&protocolType=token_1155`;
 		const resp = await fetch(url, {
 			method: 'GET',
 			headers: { 'Ok-Access-Key': '4dc070a9-44a6-474c-afc2-e8976eae75b7' }
@@ -58,13 +65,97 @@ const HomeScreen = () => {
 	// From there we will display that event and also every ticket that we own for that event.
 	// what the hell have I got myself into
 
+	const getEventNameFromId = async (id: bigint) => {
+		const publicViemClient = client.viem.createPublicClient({
+			chain: polygonAmoy
+		});
+
+		if (publicViemClient) {
+			const data = (await publicViemClient.readContract({
+				abi: ContractABI,
+				address: ContractAddress,
+				functionName: 'get_event_description',
+				args: [id]
+			})) as string;
+
+			return data;
+		} else {
+			console.log('if you see this something bad happened');
+			return '';
+		}
+	};
+
+	const getEventByUUID = async (UUID: string) => {
+		const resp = await fetch(
+			`${process.env.EXPO_PUBLIC_API_BASEURL}/user/events?ID=${UUID}`,
+			{
+				method: 'GET',
+				headers: { Authorization: `Bearer ${client.auth.token}` }
+			}
+		);
+
+		if (!resp.ok) {
+			console.log('BAD RESP');
+			return 'There was an error fetching data';
+		}
+		return await resp.json();
+	};
+
+	async function tmp() {
+		const tmp = await getOwnedTicketIds();
+
+		const ids = tmp as bigint[];
+
+		console.log('woah');
+
+		const event_names = Array(ids.length).fill('') as string[];
+		let iterator = 0;
+		for (const id in ids) {
+			event_names[iterator] = await getEventNameFromId(BigInt(id));
+			iterator += 1;
+		}
+
+		// console.log(event_names);
+
+		const event_data: string[] = [];
+
+		for (let i = 0; i < event_names.length; i++) {
+			// console.log(event_names[i]);
+			const uuid = event_names[i].split(' ')[5];
+			// console.log('uuid',uuid);
+			event_data.push(await getEventByUUID(uuid));
+		}
+
+		// for (const event in event_names) {
+		// 	console.log('event', event)
+		// 	const uuid = event.split(" ");
+		// 	console.log('uuid',uuid);
+		// 	// event_data.push(await getEventByUUID(uuid))
+		// }
+
+		console.log(event_data);
+
+		return <Button title="hello world" />;
+	}
+
+	// function EventHandler() {
+
+	// 	tmp().then(result => {
+	// 		set = true;
+	// 		return result;
+	// 	})
+
+	// 	return <Text>yay</Text>
+	// };
+
 	return (
 		<View
 			style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
 		>
 			{/* <Text>Home Screen</Text> */}
 			{/* <Button title="Go to Profile" onPress={() => navigation.navigate("Profile" as never)}/> */}
-			<Button title="test" onPress={getOwnedTicketIds} />
+			<Button title="test" onPress={tmp} />
+			{/* <EventHandler /> */}
 		</View>
 	);
 };
