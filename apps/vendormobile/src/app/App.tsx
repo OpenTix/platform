1f0cb12eb1662e9a3d30d0f5ea305b4ec3a69739
CalebKornegay <caleb.kornegay@gmail.com>
2025-03-31 21:43:44 -0400

    fix pop-up when already logged in, added logout without extra profile popup after, and profile button

--- apps/vendormobile/src/app/App.tsx
+++ apps/vendormobile/src/app/App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Text, Pressable, SafeAreaView, View } from 'react-native';
 import { useDynamic } from '../hooks/DynamicSetup';
 import EventDetails from '../screens/EventDetails';
 import Events from '../screens/Events';
@@ -13,33 +14,107 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
 	const client = useDynamic();
-
-	// if (client.auth.token === null) client.ui.auth.show();
+	const [show, setShow] = useState(false);
 
 	client.auth.on('authFailed', () => {
 		console.log('User failed to login');
+		client.ui.auth.show();
 	});
 	client.auth.on('authSuccess', (user) => {
 		console.log('User logged in', user);
+		client.ui.auth.hide();
 	});
 	client.ui.on('authFlowCancelled', () => {
 		console.log('User cancelled the flow not cool');
 		client.ui.auth.show();
 	});
+	client.auth.on('loggedOut', () => {
+		client.ui.auth.show();
+	});
 
 	useEffect(() => {
-		if (client.auth.token != null) {
+		if (!show) return;
+		if (client.auth.token !== null) {
 			client.ui.auth.hide();
 		} else {
 			client.ui.auth.show();
 		}
-	}, [client.auth.token]);
+	}, [show]);
+
+	useEffect(() => {
+		setTimeout(() => setShow(true), 1500);
+	}, [setShow]);
 
 	return (
 		<NavigationContainer>
 			<client.reactNative.WebView />
 			<Stack.Navigator initialRouteName="Home">
-				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen
+					name="Home"
+					component={Home}
+					options={{
+						headerRight: () => (
+							<SafeAreaView>
+								<View
+									style={{
+										flex: 1,
+										flexDirection: 'row',
+										columnGap: 5
+									}}
+								>
+									<Pressable
+										style={{
+											backgroundColor: 'white',
+											borderRadius: 20, // Make it round
+											padding: 7,
+											elevation: 5, // Shadow for Android
+											shadowColor: '#000', // Shadow for iOS
+											shadowOffset: {
+												width: 0,
+												height: 2
+											},
+											shadowOpacity: 0.3,
+											shadowRadius: 3,
+											alignItems: 'center',
+											justifyContent: 'center'
+										}}
+										onPressOut={() => {
+											if (client?.auth?.token !== null) {
+												client.ui.userProfile.show();
+											}
+										}}
+									>
+										<Text>Profile</Text>
+									</Pressable>
+									<Pressable
+										style={{
+											backgroundColor: 'white',
+											borderRadius: 20, // Make it round
+											padding: 7,
+											elevation: 5, // Shadow for Android
+											shadowColor: '#000', // Shadow for iOS
+											shadowOffset: {
+												width: 0,
+												height: 2
+											},
+											shadowOpacity: 0.3,
+											shadowRadius: 3,
+											alignItems: 'center',
+											justifyContent: 'center'
+										}}
+										onPressOut={() => {
+											if (client?.auth?.token !== null) {
+												client.auth.logout();
+											}
+										}}
+									>
+										<Text>Logout</Text>
+									</Pressable>
+								</View>
+							</SafeAreaView>
+						)
+					}}
+				/>
 				<Stack.Screen
 					name="Events"
 					// @ts-expect-error This is valid code, but typescript doesn't like it
