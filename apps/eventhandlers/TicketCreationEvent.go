6ed13ea42bf07f9cac87707b864ce41fa01a5b13
Connor Gannaway <charlesconnorgannaway@gmail.com>
2025-04-03 17:29:40 -0400

    add ticket creation handling in event handler

--- apps/eventhandlers/TicketCreationEvent.go
+++ apps/eventhandlers/TicketCreationEvent.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"context"
-	"log"
+	"encoding/json"
+	"errors"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/opentix/platform/packages/gohelpers/packages/database"
+	"github.com/opentix/platform/packages/gohelpers/packages/query"
 )
+
+type TicketCreateSNSMessageBody struct {
+	Event string `json:"Event"`
+	Contract string `json:"Contract"`
+	TicketMin int `json:"TicketMin"`
+	TicketMax int `json:"TicketMax"`
+}
 
 var connStr string
 
@@ -17,11 +29,66 @@ func init() {
 
 func HandleSQSEvent(ctx context.Context, sqsEvent events.SQSEvent) error {
 	// log the event
-	for _, record := range sqsEvent.Records {
-		log.Println("Received message: ", record)
-		log.Println("Message body: ", record.Body)
+	conn, err := database.ConnectToDatabase(ctx, connStr)
+	if err != nil {
+		log.Error("Error connecting to database: ", err)
+		return err
 	}
+	defer conn.Close(ctx)
+	queries := query.New(conn)
 
+	for _, record := range sqsEvent.Records {
+		var message TicketCreateSNSMessageBody
+		err := json.Unmarshal([]byte(record.Body), &message)
+		if err != nil {
+			log.Error("Error unmarshalling SNS message: ", err)
+			continue
+		}
+		
+		// get event from database, make sure it exists
+		eventUUID, err := uuid.Parse(message.Event)
+		if err != nil {
+			log.Error("Error parsing event UUID: ", err)
+			continue
+		}
+		event, err := queries.GetEventByUuid(ctx, eventUUID)
+		if err != nil {
+			log.Error("Error getting event from database: ", err)
+			continue
+		}
+		if event.ID == uuid.Nil {
+			log.Error("Event not found in database: ", message.Event)
+			continue
+		}
+
+		for i := message.TicketMin; i <= message.TicketMax; i++ {
+			// check if ticket is already in the database
+			_, err := queries.GetTicket(ctx, query.GetTicketParams{
+				Event: event.Pk, 
+				TicketID: int32(i),
+			})
+			if err != nil {
+				if errors.Is(err, pgx.ErrNoRows) {
+					// Ticket does not exist
+					_, err = queries.AddTicket(ctx, query.AddTicketParams{
+						Event: event.Pk,
+						TicketID: int32(i),
+						Contract: message.Contract,
+					})
+					if err != nil {
+						log.Error("Error adding ticket to database: ", err)
+						continue
+					}
+				} else {
+					log.Error("Error getting ticket from database: ", err)
+					continue
+				}
+			} else {
+				log.Info("Ticket already exists, skipping ticket ID: ", i)
+				continue
+			}
+		}
+	}
 	return nil
 }
 
