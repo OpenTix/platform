5923e39a17ca2927b54e4505eea1fee0995d9569
Christopher Canaday <chrislcanaday@gmail.com>
2025-04-03 23:31:22 -0400

    Remove large comment

--- apps/userweb/src/views/Profile.tsx
+++ apps/userweb/src/views/Profile.tsx
@@ -1,30 +1,3 @@
-// import { useDynamicContext, useIsLoggedIn } from '@dynamic-labs/sdk-react-core';
-// export default function Profile() {
-// 	const isLoggedIn = useIsLoggedIn();
-// 	const { primaryWallet } = useDynamicContext();
-// 	return (
-// 		<>
-// 			<h1>Profile</h1>
-// 			{isLoggedIn ? (
-// 				<>
-// 					<p>Welcome to your profile page!</p>
-// 					{primaryWallet ? (
-// 						<>
-// 							<p>{primaryWallet.id}</p>
-// 							<p>{primaryWallet.key}</p>
-// 							<p>{primaryWallet.address}</p>
-// 							<p>{primaryWallet.chain}</p>
-// 						</>
-// 					) : (
-// 						<p>Trouble Loading Primary Wallet.</p>
-// 					)}
-// 				</>
-// 			) : (
-// 				<p>You are not logged in.</p>
-// 			)}
-// 		</>
-// 	);
-// }
 import { isEthereumWallet } from '@dynamic-labs/ethereum';
 import {
 	CopyIcon,
