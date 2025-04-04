9956eedd7143e59be5fc48f46b1dea38109be864
CalebKornegay <caleb.kornegay@gmail.com>
2025-04-04 12:33:47 -0400

    Aaron or Andrew overwrote my changes to this file for some fuckall reason so I'm fixing it

--- apps/vendorweb/src/components/VendorTable.tsx
+++ apps/vendorweb/src/components/VendorTable.tsx
@@ -2,9 +2,6 @@ import { Event, EVENT_KEYS, Venue, VENUE_KEYS } from '@platform/types';
 import { Text, Table, Box, ScrollArea } from '@radix-ui/themes';
 import { useNavigate } from 'react-router-dom';
 
-// you'll grab the baseurl and then I believe it's /vendor/venues and /vendor/events
-// process.env.NX_PUBLIC_API_BASEURL
-
 interface tableData {
 	rowData: Event[] | Venue[];
 	tableType: 'event' | 'venue';
@@ -22,26 +19,61 @@ export default function VendorTable({ rowData, tableType }: tableData) {
 	}
 
 	for (const label in labels) {
-		header.push(
-			<Table.ColumnHeaderCell key={`${label}`}>
-				<Text>{labels[label]}</Text>
-			</Table.ColumnHeaderCell>
-		);
+		switch (labels[label]) {
+			case 'Pk':
+			case 'ID':
+			case 'Vendor':
+			case 'Venue':
+			case 'Photo':
+				continue;
+			default:
+				header.push(
+					<Table.ColumnHeaderCell>
+						<Text>
+							{labels[label] === 'TransactionHash'
+								? 'Minted'
+								: labels[label]}
+						</Text>
+					</Table.ColumnHeaderCell>
+				);
+		}
 	}
 	const tableHeader = <Table.Row>{header}</Table.Row>;
 
 	const tableRows = (rowData ?? []).map((row, idx) => {
 		const tableRow = [];
 		for (const label in row) {
-			tableRow.push(
-				<Table.Cell key={`${label}:${row[label as keyof typeof row]}`}>
-					<Text>
-						{label === 'EventDatetime'
-							? `${new Date(row[label as keyof typeof row])}`
-							: row[label as keyof typeof row]}
-					</Text>
-				</Table.Cell>
-			);
+			switch (label) {
+				case 'Pk':
+				case 'ID':
+				case 'Vendor':
+				case 'Venue':
+				case 'Photo':
+					continue;
+				case 'TransactionHash': {
+					const val = row[label as keyof typeof row];
+					tableRow.push(
+						<Table.Cell>
+							<Text>
+								{val === '' || val === null || val === undefined
+									? '❌'
+									: '✅'}
+							</Text>
+						</Table.Cell>
+					);
+					break;
+				}
+				default:
+					tableRow.push(
+						<Table.Cell>
+							<Text>
+								{label === 'EventDatetime'
+									? `${new Date(row[label as keyof typeof row]).toDateString()}`
+									: row[label as keyof typeof row]}
+							</Text>
+						</Table.Cell>
+					);
+			}
 		}
 
 		return (
@@ -55,7 +87,7 @@ export default function VendorTable({ rowData, tableType }: tableData) {
 	});
 
 	return (
-		<Box>
+		<Box style={{ paddingBottom: '5px' }}>
 			<ScrollArea>
 				<Table.Root>
 					<Table.Header>{tableHeader}</Table.Header>
