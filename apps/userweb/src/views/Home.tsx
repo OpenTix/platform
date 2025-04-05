b77cef1c4fce9f469d4147bb432578a0a4e0df1a
Caleb Kornegay <108814834+CalebKornegay@users.noreply.github.com>
2025-04-05 11:50:20 -0400

    Zip Code Radius (#47)
    * Adding geolocation search for zip code to show 'Near You' events
    
    * Adding geolocation search for zip code to show 'Near You' events
    
    * Update apps/userweb/src/views/Home.tsx
    
    * Fix build oops
    
    * Adding geolocation search for zip code to show 'Near You' events with multiple zip codes
    
    * Add date to query to fix stale events
    
    * Change date format to always show year, move day of week below with time
    
    * Increase returned zips from 3 -> 5
    
    * Aaron or Andrew overwrote my changes to this file for some fuckall reason so I'm fixing it
    
    * Update display: block to fix image not formatting to card correctly on EventDetailsPage
    
    * Moving activeTab to sessionStorage because going back causes the tab to change if you're on venues which is very annoying. Asked someone to fix in their PR but that didn't happen, not surprising

--- apps/userweb/src/views/Home.tsx
+++ apps/userweb/src/views/Home.tsx
@@ -1,73 +1,107 @@
-import { AllEventTypesArray } from '@platform/types';
-import { Box, Container, Flex } from '@radix-ui/themes';
-import { useState, useEffect } from 'react';
-import { useSessionStorage } from 'usehooks-ts';
+import { AllEventTypesArray, UserEventResponse } from '@platform/types';
+import { Box, Card, Container, Flex, Text } from '@radix-ui/themes';
+import { useState, useEffect, useCallback, useRef } from 'react';
+import { EventCard } from '../components/EventCard';
 import EventRow from '../components/EventRow';
 
 export default function Home() {
-	const [shouldFetch, setShouldFetch] = useSessionStorage(
-		'ShouldFetch',
-		true
-	);
-	const [zip, setZip] = useSessionStorage('Zip', '');
 	const [cards, setCards] = useState<React.ReactNode>(null);
+	const near = useRef<React.ReactNode[]>([]);
+	const [shouldShow, setShouldShow] = useState<boolean>(true);
+	const [nearReady, setNearReady] = useState<boolean>(false);
+
+	const getEvents = useCallback(async (postcode: string) => {
+		const url = `${process.env.NX_PUBLIC_API_BASEURL}/user/events?zip=${postcode}&EventDatetime=${new Date().toISOString()}`;
+
+		const resp = await fetch(url, {
+			method: 'GET'
+		});
+
+		if (!resp.ok) {
+			console.error('There was an error fetching data');
+			return (
+				<Card>
+					<Text>There was an error fetching data</Text>
+				</Card>
+			);
+		}
+
+		const data = await resp.json();
+
+		return await (data && data.length !== 0
+			? (near.current = [
+					...near.current,
+					data.map((event: UserEventResponse, idx: number) => (
+						<EventCard key={`${idx}:${event.Name}`} event={event} />
+					))
+				])
+			: undefined);
+	}, []);
 
 	useEffect(() => {
-		setShouldFetch(true);
 		navigator?.geolocation?.getCurrentPosition(
 			async (position) => {
 				const lat = position?.coords?.latitude;
 				const lon = position?.coords?.longitude;
 				const resp = await fetch(
-					`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=18&addressdetails=1`
+					`${process.env.NX_PUBLIC_API_BASEURL}/user/zips?Radius=50&Latitude=${lat}&Longitude=${lon}`,
+					{
+						method: 'GET',
+						referrer: 'https://client.dev.opentix.co'
+					}
 				);
-				const json = await resp?.json();
-				setZip(json?.address?.postcode ?? '');
+				const json = await resp.json();
+				json?.postcodes?.forEach(async (pc: number) => {
+					await getEvents(pc.toString());
+					if (near.current.length >= 5) {
+						setTimeout(() => setNearReady(true), 750);
+						return;
+					}
+				});
+				setTimeout(() => setNearReady(true), 750);
 			},
 			(error) => {
 				console.error('Geolocation error:', error);
 			}
 		);
-	}, [setShouldFetch, setZip]);
+	}, [getEvents]);
 
 	useEffect(() => {
-		async function getEvents() {
-			setShouldFetch(false);
-
+		async function showEvents() {
 			setCards(
-				<>
-					{zip !== '' ? (
-						<EventRow
-							key={'Near You'}
-							zip={zip}
-							type={'Near You'}
-							name={''}
-							cost={'1000000'}
-							eventDate={new Date().toISOString()}
-						/>
-					) : null}
-					{AllEventTypesArray.map(
-						(eventType: string, idx: number) => (
-							<EventRow
-								key={`${idx}:${eventType}`}
-								zip={''}
-								type={eventType}
-								name={''}
-								cost={'1000000'}
-								eventDate={new Date().toISOString()}
-							/>
-						)
-					)}
-				</>
+				AllEventTypesArray.map((eventType: string, idx: number) => (
+					<EventRow
+						key={`${idx}:${eventType}`}
+						zip={''}
+						type={eventType}
+						name={''}
+						cost={'1000000'}
+						eventDate={new Date().toISOString()}
+					/>
+				))
 			);
 		}
-		getEvents();
-	}, [shouldFetch, setShouldFetch, zip]);
+		if (shouldShow) {
+			setShouldShow(false);
+			showEvents();
+		}
+	}, [shouldShow, setShouldShow, nearReady]);
 
 	return (
 		<Flex align="start" gap="4" style={{ marginTop: '10px' }}>
 			<Container style={{ alignSelf: 'center' }} size={'4'}>
 				<Box style={{ maxWidth: '90vw', padding: '16px 16px' }}>
+					{nearReady && near.current.length > 0 ? (
+						<EventRow
+							key={'Near You'}
+							zip={''}
+							type={'Near You'}
+							name={''}
+							cost={'1000000'}
+							eventDate={new Date().toISOString()}
+							passedData={near.current.slice(0, 5)}
+						/>
+					) : null}
 					{cards}
 				</Box>
 			</Container>
