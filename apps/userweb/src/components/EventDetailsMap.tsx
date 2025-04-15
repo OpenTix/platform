d564bd30036e53a6147ebf708ad4165522538420
Connor Gannaway <charlesconnorgannaway@gmail.com>
2025-04-14 21:04:38 -0400

    update map tiles to carto voyager

--- apps/userweb/src/components/EventDetailsMap.tsx
+++ apps/userweb/src/components/EventDetailsMap.tsx
@@ -75,8 +75,10 @@ export default function EventDetailsMap({ data }: EventDetailsMapProps) {
 				style={{ width: '100%', height: '100%' }}
 			>
 				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+					url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+					subdomains="abcd"
+					maxZoom={20}
 				/>
 				{/* Drop a pin at the center */}
 				<Marker position={[lat, lon]}>
